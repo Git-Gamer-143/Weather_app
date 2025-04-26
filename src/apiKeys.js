f49958ade488b6175050df81e8ad09bfe604b9ab
Git-Gamer-143 <shaurya45468@gmail.com>
2025-04-26 15:01:58 +0530

    done

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "6427cecd2a5b7b5ad538d9915f4d066e",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
